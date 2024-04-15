ec8dcffb00119ed8f0a3de580b7d5d37ce2d7787
careengineering <careengineeringx@gmail.com>
2024-04-15 21:37:24 +0300

    first

--- jetbul/ucus/admin.py
+++ jetbul/ucus/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
 
 # Register your models here.
+
+from.models import Airport
+
+class AirportAdmin(admin.ModelAdmin):
+    list_display = ("isim","ulke","sehir","kod")
+    search_fields = ("ulke","sehir")
+    list_filter = ("ulke", "sehir")
+
+admin.site.register(Airport,AirportAdmin)
--- jetbul/ucus/models.py
+++ jetbul/ucus/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+class Airport(models.Model):
+    isim = models.CharField(max_length=180) # Charfield= kısa metinsel ifade, max_lenght belirtilmesi
+    sehir = models.CharField(max_length=50)
+    ulke = models.CharField(max_length=80)
+    kod = models.CharField(max_length=8)
+
+    def __str__(self):
+        return self.isim #listede object yerine isim görünmesi için
+
+
+
